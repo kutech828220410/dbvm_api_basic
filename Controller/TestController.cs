bbd5bbf7402b2c6ac41c8d3a9ea7f8323eea3a99
HS-kmuh <HS@HS>
2025-01-17 15:56:59 +0800

    加入專案檔案。

---
+++ Controller/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IBM.Data.DB2.Core;
+using System.Data;
+using System.Configuration;
+using Basic;
+using Oracle.ManagedDataAccess.Client;
+namespace DB2VM
+{
+    [Route("dbvm/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return Basic.Net.WEBApiGet($"http://127.0.0.1:4433/api/test");
+        }
+
+
+    }
+}
